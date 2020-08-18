5f631e15c727374776224565f30c06cb5ce5128d
ogulle <oigulle@gmail.com>
2020-08-17 21:38:53 -0500

    Day 11

--- src/test/java/test/day9_testbase_propertiesdriverUtil/DriverUtilsPractice.java
+++ src/test/java/test/day9_testbase_propertiesdriverUtil/DriverUtilsPractice.java
@@ -3,12 +3,14 @@ package test.day9_testbase_propertiesdriverUtil;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import utilities.ConfigurationReader;
 import utilities.Driver;
 
 public class DriverUtilsPractice {
 
+    @Ignore
     @Test
     public void driver_practice(){
         Driver.getDriver().get("https://www.google.com");
@@ -17,8 +19,24 @@ public class DriverUtilsPractice {
         googleSearch.sendKeys(ConfigurationReader.getProperty("testdata")+ Keys.ENTER);
 
 
+    }
 
+    @Test
+    public void singleton_practice(){
+        String str1 = Singleton.getWord();
+        System.out.println("str1= " + str1);
 
+        String str2 = Singleton.getWord();
+        System.out.println("str2  " +str2);
+
+        String str3 = Singleton.getWord();
+        System.out.println("str3 = " + str3);
+    }
+
+    @Test
+    public void singleton_practice2(){
+        String str4 = Singleton.getWord();
+        System.out.println("str4 = " + str4);
     }
 
 
